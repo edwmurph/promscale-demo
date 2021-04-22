4d2c8c9d26b88fc584e2401cdb79485aae75b808
emurphy <emurphy@starry.com>
2021-04-22 07:44:42 -0400

    HA promscale

--- app/send-telemetry.js
+++ app/send-telemetry.js
@@ -2,25 +2,35 @@ const axios = require('axios');
 
 const jitter = 20;
 
+const promscales = [
+  'http://localhost:9201/write',
+  'http://localhost:9202/write'
+];
+
 const sendTelemetry = async() => {
   const slop = ( jitter / 2 ) - ( Math.random() * jitter );
 
-  await axios({
-    url: 'http://localhost:9201/write',
-    method: 'post',
-    data: {
-      labels: {
-        '__name__': 'custom_metric_pushed',
-        test: 'a'
+  // send telemetry to all promscale instances but it's only written to postgres once.
+  // we could create an elb that forwards to all promscale instances so we only have to
+  // send to a single endpoint
+  for ( const url of promscales ) {
+    await axios({
+      url,
+      method: 'post',
+      data: {
+        labels: {
+          '__name__': 'custom_metric_pushed',
+          test: 'a'
+        },
+        samples: [
+          [ new Date().getTime(), 100 + slop ]
+        ]
       },
-      samples: [
-        [ new Date().getTime(), 100 + slop ]
-      ]
-    },
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
 };
 
 setInterval( sendTelemetry, 2000 );
