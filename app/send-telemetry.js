aa2acd86e945e507b7d4aea2d97cb26699b8b452
emurphy <emurphy@starry.com>
2021-04-22 13:00:17 -0400

    use new HA setup

--- app/send-telemetry.js
+++ app/send-telemetry.js
@@ -12,28 +12,33 @@ const promscales = [
 ];
 
 const sendTelemetry = async() => {
-  const slop = ( jitter / 2 ) - ( Math.random() * jitter );
+  try {
+    const slop = ( jitter / 2 ) - ( Math.random() * jitter );
 
-  // send telemetry to all promscale instances but it's only written to postgres once.
-  // we could create an elb that forwards to all promscale instances so we only have to
-  // send to a single endpoint
-  for ( const url of promscales ) {
-    await axios({
-      url,
-      method: 'post',
-      data: {
-        labels: {
-          '__name__': 'custom_metric_pushed',
-          test: 'a'
+    // send telemetry to all promscale instances but it's only written to postgres once.
+    // we could create an elb that forwards to all promscale instances so we only have to
+    // send to a single endpoint
+    for ( const url of promscales ) {
+      await axios({
+        url,
+        method: 'post',
+        data: {
+          labels: {
+            '__name__': 'custom_metric_pushed',
+            cluster: 'app',
+            '__replica__': 'app1'
+          },
+          samples: [
+            [ new Date().getTime(), 100 + slop + (triggerAlert ? 50 : -50) ]
+          ]
         },
-        samples: [
-          [ new Date().getTime(), 100 + slop + (triggerAlert ? 50 : -50) ]
-        ]
-      },
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    }
+  } catch ( ex ) {
+    console.log( ex );
   }
 };
 
